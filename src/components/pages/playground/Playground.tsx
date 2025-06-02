c5bf2dfd0a4dad3f1bce51db8c4208d434016842
Damian <damianp.150898@gmail.com>
2025-06-02 19:41:21 +0200

    Styling and playground shinenigans

--- src/components/pages/playground/Playground.tsx
+++ src/components/pages/playground/Playground.tsx
@@ -3,20 +3,39 @@ import "./Playground.css";
 
 const Playground = () => {
    const [, setUpdate] = useState(1);
+   const [text, setText] = useState<string>("");
    const test = "abc+";
 
-   if (!test.match(/d/)) {
-      console.log("found");
-   }
+   const handleType = (event: React.ChangeEvent<HTMLInputElement>) => {
+      let newValue = event.target.value.replace(/\D/g, "");
+      let newValueArray = newValue.split("");
+
+      if (newValue.length > 3) {
+         newValueArray.splice(3, 0, ")");
+         newValueArray.splice(0, 0, "(");
+      }
+
+      if (newValue.length > 6) {
+         newValueArray.splice(8, 0, "-");
+      }
+
+      newValueArray.at(-1)?.match(/[\D]/) ?? setText(newValueArray.join(""));
+   };
 
    return (
       <div className="Playground">
-         <div
-            className="test"
-            data-theme="aha"
+         <input
+            type="text"
+            value={text}
+            onInput={handleType}
+            className="Input"
+         />
+         <button
+            onClick={() => setText("")}
+            className="reset"
          >
-            <div className="test2"></div>
-         </div>
+            Reset
+         </button>
       </div>
    );
 };
